70b331fa5def5a9742751061850be1cf6986c6ed
Lucas Marcelino <lucas_marcelino2000@hotmail.com>
2022-07-31 10:39:11 -0300

    Adicionando de teste de falha no registro

--- spec/request/api/user_spec.rb
+++ spec/request/api/user_spec.rb
@@ -15,5 +15,18 @@ RSpec.describe 'User', :type => :request do
       expect(json_response['user']).to eq('Rodolfo Alves')
       expect(json_response['email']).to eq('rodolfo@email.com')
     end
+
+    it 'with invalid data and its failed' do
+      user_params = {user: {email: "", password: "1234567", name: "Rodolfo Alves",
+                     phone: '(47) 9 9474-7178', city: '', address: 'Rua Mafra, 247'}}
+      post('/users', params: user_params)
+
+      expect(response.status).to eq(422)
+      expect(response.content_type).to include('application/json')
+      json_response = JSON.parse(response.body)
+      expect(json_response['message']).to eq('Its failed')
+      expect(json_response['errors']).to include("Email can't be blank")
+      expect(json_response['errors']).to include("City can't be blank")
+    end
   end
 end
